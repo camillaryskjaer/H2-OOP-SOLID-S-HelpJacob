58eb55e653fc8858b51e2892a956f90e8d224c52
camillaryskjaer <camr@zbc.dk>
2021-06-22 17:35:20 +0200

    Update SendSmtp.cs

--- SendSmtp.cs
+++ SendSmtp.cs
@@ -10,33 +10,18 @@ namespace HelpJacob
     public class SendSmtp: ISender
     {
 
-        public void SendMessage(MessageCarrier type, Message m)
+        public void SendMessage( Message m)
         {
-            //herinde sendes der en email ud til modtageren
-            if (type.Equals(MessageCarrier.Smtp))
-            {
+         
                 //her implementeres alt koden til at sende via Smtp
-            }
-
-            if (type.Equals(MessageCarrier.VMessage))
-            {
-                //her implementeres alt koden til at sende via VMessage
-            }
+           
         }
 
-        public void SendMessageToAll(MessageCarrier type, string[] to, Message m)
+        public void SendMessageToAll(Message m)
         {
-            if (type.Equals(MessageCarrier.Smtp))
-            {
+            
                 //her implementeres alt koden til at sende via Smtp
-            }
-
-
-
-            if (type.Equals(MessageCarrier.VMessage))
-            {
-                //her implementeres alt koden til at sende via VMessage
-            }
+            
         }
     }
 }
